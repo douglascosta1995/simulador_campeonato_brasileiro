079462cd3b97c4f4cc58ee066b3657d80e5e9358
Douglas <douglascastro2010@hotmail.com>
2022-08-19 23:38:55 +0200

    Modificar Instance attribute name info_times

--- campeonato.py
+++ campeonato.py
@@ -7,29 +7,31 @@ from simulador_de_partida import Simulador_de_Partidas
 
 class Campeonato(object):
     def __init__(self, info_times_inicial):
-        self.info_times_inicial = info_times_inicial
+        self.info_times = info_times_inicial
         pass
 
     def informacao_dos_times(self, time, pontos, vitorias, empates, derrotas, gp, gc, sg):
-        self.info_times_inicial[time]['pontos'] += pontos
-        self.info_times_inicial[time]['vitorias'] += vitorias
-        self.info_times_inicial[time]['empates'] += empates
-        self.info_times_inicial[time]['derrotas'] += derrotas
-        self.info_times_inicial[time]['gols_pro'] += gp
-        self.info_times_inicial[time]['gols_contra'] += gc
-        self.info_times_inicial[time]['saldo_de_gols'] += sg
+        self.info_times[time]['pontos'] += pontos
+        self.info_times[time]['vitorias'] += vitorias
+        self.info_times[time]['empates'] += empates
+        self.info_times[time]['derrotas'] += derrotas
+        self.info_times[time]['gols_pro'] += gp
+        self.info_times[time]['gols_contra'] += gc
+        self.info_times[time]['saldo_de_gols'] += sg
 
         #print(self.info_times_inicial)
 
     def rodadas(self):
-        key = list(self.info_times_inicial)
+        key = list(self.info_times)
+        # colocar in a loop pra simular as rodadas
         resultado = Simulador_de_Partidas(key[0], key[1]).simular()
         Campeonato.informacao_dos_times(self, key[0], resultado[0], resultado[1], resultado[2], resultado[3], resultado[4],
                                         resultado[5], resultado[6])
         Campeonato.informacao_dos_times(self, key[1], resultado[7], resultado[8], resultado[9], resultado[10], resultado[11],
                                         resultado[12], resultado[13])
 
-        print(self.info_times_inicial)
+        print(self.info_times)
+        # criar tabela de classificação
 
         '''
         for i in range(0, 10):
