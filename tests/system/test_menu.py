e418ec58bb94f6efabf61db19f36379ab8165257
Douglas <douglascastro2010@hotmail.com>
2022-09-19 23:19:10 +0200

    Adicionando pasta para guardar os testes de sistema e adicionando testes iniciais para o menu

---
+++ tests/system/test_menu.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+from unittest.mock import patch, call
+import menu
+
+MENSAGEM_INICIAL = 'Seja bem vindo ao Campeonato'
+MENSAGEM_RESPOSTA_INVALIDA = 'Resposta inválida. Tente novamente'
+MENSAGEM_RESPOSTA_INVALIDA_NAO_NUMERICA = 'Somente números são aceitos. Tente novamente'
+MENSAGEM_ENCERRANDO_SIMULADOR = 'Até logo!! :)'
+
+class MenuTest(TestCase):
+    def test_menu_inicial_resposta_numerica_invalida(self):
+        """
+        Este caso de teste tem como finalidade checar que quando o simulador é iniciado, o menu inicial será
+        chamado e caso o usuário forneça inputs numéricos diferentes dos esperados, uma mensagem de error
+        será exibida ao usuário.
+        Para fins de teste, Logo após é fornecido ao menu a informação de encerrar o programa.
+        """
+        with patch('builtins.input') as mocked_input:
+            with patch('builtins.print') as mocked_print:
+                mocked_input.side_effect = (5, 5, 0)
+                menu.Menu.menu_inicial(0)
+                self.assertEqual(mocked_print.mock_calls, [call(MENSAGEM_INICIAL),
+                                                           call(MENSAGEM_RESPOSTA_INVALIDA),
+                                                           call(MENSAGEM_ENCERRANDO_SIMULADOR)])
+
+    def test_menu_inicial_resposta_nao_numerica(self):
+        """
+        Este caso de teste tem como finalidade checar que quando o simulador é iniciado, o menu inicial será
+        chamado e caso o usuário forneça inputs não numéricos, uma mensagem de error será exibida ao usuário.
+        Para fins de teste, Logo após é fornecido ao menu a informação de encerrar o programa.
+        """
+        with patch('builtins.input') as mocked_input:
+            with patch('builtins.print') as mocked_print:
+                mocked_input.side_effect = ('A', ' ', 0)
+                menu.Menu.menu_inicial(0)
+                self.assertEqual(mocked_print.mock_calls, [call(MENSAGEM_INICIAL),
+                                                           call(MENSAGEM_RESPOSTA_INVALIDA_NAO_NUMERICA),
+                                                           call(MENSAGEM_RESPOSTA_INVALIDA_NAO_NUMERICA),
+                                                           call(MENSAGEM_ENCERRANDO_SIMULADOR)])
+
+    def test_menu_inicial_resposta_correta(self):
+        pass
+
